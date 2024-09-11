3ae1c59ac9582efdf7df6921a2ea4b96fd6fb4b5
Hamdi Souissi <hamdi.s@pixelstrade.com>
2024-09-11 12:19:58 +0200

    first commit

---
+++ src/test/java/com/testreservations/testreservation/TestreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testreservations.testreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
